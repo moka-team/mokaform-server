d21e14ebb5a294514f76c9feba3ec30b1504c471
HyeonseoJUNG <windymay1920@gmail.com>
2022-09-27 09:53:32 +0900

    init project

---
+++ src/main/java/com/mokaform/mokaformserver/MokaformServerApplication.java
@@ -0,0 +1,13 @@
+package com.mokaform.mokaformserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MokaformServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MokaformServerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/mokaform/mokaformserver/MokaformServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mokaform.mokaformserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MokaformServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
