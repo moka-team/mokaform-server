dec6ae8d7b3aaa3cc52f9577b3687273fcb1abb0
HyeonseoJUNG <windymay1920@gmail.com>
2022-10-04 02:55:53 +0900

    feat: BaseEntity 생성 및 auditing 적용

--- src/main/java/com/mokaform/mokaformserver/MokaformServerApplication.java
+++ src/main/java/com/mokaform/mokaformserver/MokaformServerApplication.java
@@ -2,12 +2,14 @@ package com.mokaform.mokaformserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class MokaformServerApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(MokaformServerApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(MokaformServerApplication.class, args);
+    }
 
 }
