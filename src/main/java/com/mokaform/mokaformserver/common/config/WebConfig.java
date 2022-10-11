0ab33f76c448c58619125719286036b881b372ea
HyeonseoJUNG <windymay1920@gmail.com>
2022-10-11 09:58:58 +0900

    feat: cors 설정

---
+++ src/main/java/com/mokaform/mokaformserver/common/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.mokaform.mokaformserver.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/v1/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
