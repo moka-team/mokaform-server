233b6393d60e12c7abf6088025f655d9cd04897e
HyeonseoJUNG <windymay1920@gmail.com>
2022-10-14 02:50:01 +0900

    feat: 설문 생성 controller 구현 (MOKA-56)

--- src/main/java/com/mokaform/mokaformserver/survey/controller/SurveyController.java
+++ src/main/java/com/mokaform/mokaformserver/survey/controller/SurveyController.java
@@ -1,32 +1,58 @@
 package com.mokaform.mokaformserver.survey.controller;
 
+import com.mokaform.mokaformserver.common.exception.ApiException;
+import com.mokaform.mokaformserver.common.exception.errorcode.CommonErrorCode;
 import com.mokaform.mokaformserver.common.response.ApiResponse;
-import com.mokaform.mokaformserver.survey.dto.request.SurveySaveRequest;
+import com.mokaform.mokaformserver.survey.dto.request.SurveyCreateRequest;
 import com.mokaform.mokaformserver.survey.service.SurveyService;
+import com.mokaform.mokaformserver.user.domain.User;
+import com.mokaform.mokaformserver.user.repository.UserRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/survey")
 public class SurveyController {
-    private final SurveyService createSurveyService;
+    private final SurveyService surveyCreateService;
 
-    public SurveyController(SurveyService createSurveyService) {
-        this.createSurveyService = createSurveyService;
+    private final UserRepository userRepository;
+
+    public SurveyController(SurveyService surveyCreateService, UserRepository userRepository) {
+        this.surveyCreateService = surveyCreateService;
+        this.userRepository = userRepository;
     }
 
-    @PostMapping("/create")
-    public ResponseEntity<ApiResponse> saveSurvey(@RequestBody @Valid SurveySaveRequest request) {
-        createSurveyService.saveSurvey(request);
+    @PostMapping
+    public ResponseEntity<ApiResponse> createSurvey(@RequestBody @Valid SurveyCreateRequest request,
+                                                    @RequestParam Long userId) {
+
+        // TODO: 로그인 구현 후에 삭제
+        User user = getUser(userId);
+
+        surveyCreateService.createSurvey(request, user);
 
         return ResponseEntity.ok()
                 .body(ApiResponse.builder()
                         .message("새로운 설문 생성이 성공하였습니다.")
                         .build());
     }
+
+    // TODO: 로그인 구현 후에 삭제
+    private User getUser(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() ->
+                        new ApiException(CommonErrorCode.INVALID_PARAMETER));
+    }
+
+//    @PostMapping("/create")
+//    public ResponseEntity<ApiResponse> saveSurvey(@RequestBody @Valid SurveySaveRequest request) {
+//        createSurveyService.saveSurvey(request);
+//
+//        return ResponseEntity.ok()
+//                .body(ApiResponse.builder()
+//                        .message("새로운 설문 생성이 성공하였습니다.")
+//                        .build());
+//    }
 }
