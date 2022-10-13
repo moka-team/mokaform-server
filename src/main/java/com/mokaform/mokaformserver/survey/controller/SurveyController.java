e8e7874a05cd34b3cd7b5f16ff709c9fc0416954
HyeonseoJUNG <windymay1920@gmail.com>
2022-10-14 04:23:53 +0900

    feat: 설문 생성 api의 response data 추가 (MOKA-56)

--- src/main/java/com/mokaform/mokaformserver/survey/controller/SurveyController.java
+++ src/main/java/com/mokaform/mokaformserver/survey/controller/SurveyController.java
@@ -4,6 +4,7 @@ import com.mokaform.mokaformserver.common.exception.ApiException;
 import com.mokaform.mokaformserver.common.exception.errorcode.CommonErrorCode;
 import com.mokaform.mokaformserver.common.response.ApiResponse;
 import com.mokaform.mokaformserver.survey.dto.request.SurveyCreateRequest;
+import com.mokaform.mokaformserver.survey.dto.response.SurveyCreateResponse;
 import com.mokaform.mokaformserver.survey.service.SurveyService;
 import com.mokaform.mokaformserver.user.domain.User;
 import com.mokaform.mokaformserver.user.repository.UserRepository;
@@ -31,11 +32,12 @@ public class SurveyController {
         // TODO: 로그인 구현 후에 삭제
         User user = getUser(userId);
 
-        surveyCreateService.createSurvey(request, user);
+        SurveyCreateResponse response = surveyCreateService.createSurvey(request, user);
 
         return ResponseEntity.ok()
                 .body(ApiResponse.builder()
                         .message("새로운 설문 생성이 성공하였습니다.")
+                        .data(response)
                         .build());
     }
 
