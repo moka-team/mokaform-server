4641d6f6b30a718c2ac21d798ea32e7ea52e29f7
HyeonseoJUNG <windymay1920@gmail.com>
2022-10-17 19:12:56 +0900

    feat: querydsl을 사용한 AnswerCustomRepository 구현 (MOKA-76)

--- src/main/java/com/mokaform/mokaformserver/answer/repository/AnswerRepository.java
+++ src/main/java/com/mokaform/mokaformserver/answer/repository/AnswerRepository.java
@@ -3,5 +3,5 @@ package com.mokaform.mokaformserver.answer.repository;
 import com.mokaform.mokaformserver.answer.domain.Answer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AnswerRepository extends JpaRepository<Answer, Long> {
+public interface AnswerRepository extends JpaRepository<Answer, Long>, AnswerCustomRepository {
 }
