573d50dd885c19d624b22d57c87fe17b56dcdd79
HyeonseoJUNG <windymay1920@gmail.com>
2022-10-17 19:14:56 +0900

    feat: EssayAnswerRepository에 answerId로 entity 조회 메소드 구현 (MOKA-76)

--- src/main/java/com/mokaform/mokaformserver/answer/repository/EssayAnswerRepository.java
+++ src/main/java/com/mokaform/mokaformserver/answer/repository/EssayAnswerRepository.java
@@ -2,6 +2,13 @@ package com.mokaform.mokaformserver.answer.repository;
 
 import com.mokaform.mokaformserver.answer.domain.EssayAnswer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface EssayAnswerRepository extends JpaRepository<EssayAnswer, Long> {
+
+    @Query("SELECT ea FROM EssayAnswer ea WHERE ea.answer.answerId = :answerId")
+    Optional<EssayAnswer> findByAnswerId(@Param("answerId") Long answerId);
 }
