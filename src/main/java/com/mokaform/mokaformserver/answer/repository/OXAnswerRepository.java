fbfc67f9eaf4ad9a1226b48ecdbb9cf2a0990afe
HyeonseoJUNG <windymay1920@gmail.com>
2022-10-17 19:15:42 +0900

    feat: OXAnswerRepository에 answerId로 entity 조회 메소드 구현 (MOKA-76)

--- src/main/java/com/mokaform/mokaformserver/answer/repository/OXAnswerRepository.java
+++ src/main/java/com/mokaform/mokaformserver/answer/repository/OXAnswerRepository.java
@@ -2,6 +2,13 @@ package com.mokaform.mokaformserver.answer.repository;
 
 import com.mokaform.mokaformserver.answer.domain.OXAnswer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface OXAnswerRepository extends JpaRepository<OXAnswer, Long> {
+
+    @Query("SELECT oxa FROM OXAnswer oxa WHERE oxa.answer.answerId = :answerId")
+    Optional<OXAnswer> findByAnswerId(@Param("answerId") Long answerId);
 }
