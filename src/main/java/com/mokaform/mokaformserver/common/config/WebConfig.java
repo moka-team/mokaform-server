f715b2af9cf134374e7679bc136fc70d2ef2c1ca
HyeonseoJUNG <windymay1920@gmail.com>
2022-10-17 15:18:11 +0900

    chore: "https://mokaform-client.vercel.app" origin 추가

--- src/main/java/com/mokaform/mokaformserver/common/config/WebConfig.java
+++ src/main/java/com/mokaform/mokaformserver/common/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/v1/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "https://mokaform-client.vercel.app")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
