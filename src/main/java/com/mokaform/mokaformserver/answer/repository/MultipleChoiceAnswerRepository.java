d93b4039fab5405adcdccf0c61ebdaf771314b0a
HyeonseoJUNG <windymay1920@gmail.com>
2022-10-17 19:15:26 +0900

    feat: MultipleChoiceAnswerRepository에 answerId로 entity 조회 메소드 구현 (MOKA-76)

--- src/main/java/com/mokaform/mokaformserver/answer/repository/MultipleChoiceAnswerRepository.java
+++ src/main/java/com/mokaform/mokaformserver/answer/repository/MultipleChoiceAnswerRepository.java
@@ -2,6 +2,13 @@ package com.mokaform.mokaformserver.answer.repository;
 
 import com.mokaform.mokaformserver.answer.domain.MultipleChoiceAnswer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface MultipleChoiceAnswerRepository extends JpaRepository<MultipleChoiceAnswer, Long> {
+
+    @Query("SELECT mca FROM MultipleChoiceAnswer mca WHERE mca.answer.answerId = :answerId")
+    Optional<MultipleChoiceAnswer> findByAnswerId(@Param("answerId") Long answerId);
 }
