ba3d0d48343e1a9d2edcb5bdde496eb3695333d8
HyeonseoJUNG <windymay1920@gmail.com>
2022-10-26 11:41:04 +0900

    chore: 'https://mokaform-client.vercel.app' origin 추가

--- src/main/java/com/mokaform/mokaformserver/common/config/WebConfig.java
+++ src/main/java/com/mokaform/mokaformserver/common/config/WebConfig.java
@@ -14,7 +14,8 @@ public class WebConfig implements WebMvcConfigurer {
                         "https://mokaform-client-q6w1.vercel.app",
                         "https://startling-melba-4ca4af.netlify.app",
                         "https://feat-deploy--papaya-horse-fada9b.netlify.app",
-                        "https://mokaform-client-bnjs9ux4o-mokaform-test.vercel.app")
+                        "https://mokaform-client-bnjs9ux4o-mokaform-test.vercel.app",
+                        "https://mokaform-client.vercel.app")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
