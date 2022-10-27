fcf85fa33bfe82c8bb21cbc08b1eae2397394a94
HyeonseoJUNG <windymay1920@gmail.com>
2022-10-27 11:12:14 +0900

    feat: UserUtilService.getUser() 생성 (MOKA-50)

---
+++ src/main/java/com/mokaform/mokaformserver/common/util/UserUtilService.java
@@ -0,0 +1,24 @@
+package com.mokaform.mokaformserver.common.util;
+
+import com.mokaform.mokaformserver.common.exception.ApiException;
+import com.mokaform.mokaformserver.common.exception.errorcode.UserErrorCode;
+import com.mokaform.mokaformserver.user.domain.User;
+import com.mokaform.mokaformserver.user.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserUtilService {
+
+    private final UserRepository userRepository;
+
+    public UserUtilService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public User getUser(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new ApiException(UserErrorCode.USER_NOT_FOUND));
+    }
+}
