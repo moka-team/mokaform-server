c15d4a75ea1151bf59edd2efe697790f9f2cab3b
HyeonseoJUNG <windymay1920@gmail.com>
2022-11-17 19:53:33 +0900

    feat: 카테고리별 추천 설문 다건 조회 service 구현 (MOKA-152)

--- src/main/java/com/mokaform/mokaformserver/common/exception/errorcode/SurveyErrorCode.java
+++ src/main/java/com/mokaform/mokaformserver/common/exception/errorcode/SurveyErrorCode.java
@@ -10,7 +10,8 @@ public enum SurveyErrorCode implements ErrorCode {
     NO_PERMISSION_TO_GET_SURVEY_INFO("S004", HttpStatus.NOT_FOUND, "설문 정보를 얻을 수 없습니다."),
     NO_PERMISSION_TO_UPDATE_SURVEY("S005", HttpStatus.NOT_FOUND, "설문을 수정할 수 없습니다."),
     INVALID_START_DATE("S006", HttpStatus.BAD_REQUEST, "설문 시작일은 오늘 이전일 수 없습니다."),
-    INVALID_END_DATE("S007", HttpStatus.BAD_REQUEST, "설문 종료일은 시작일 이전일 수 없습니다.");
+    INVALID_END_DATE("S007", HttpStatus.BAD_REQUEST, "설문 종료일은 시작일 이전일 수 없습니다."),
+    INVALID_CATEGORY_TYPE("S008", HttpStatus.BAD_REQUEST, "잘못된 카테고리 종류입니다.");
 
     private final String code;
     private final HttpStatus httpStatus;
